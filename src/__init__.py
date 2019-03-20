7f8851299e79b0c62d7fe9bdfe756cd324e7a3c7
Lars Kue <lars@kuehmichel.de>
2019-03-20 15:08:52 +0100

    increased regex modularity

--- src/__init__.py
+++ src/__init__.py
@@ -11,6 +11,40 @@ def check_quit():
     return keyboard.is_pressed("alt") and keyboard.is_pressed("q")
 
 
+def clean_image(img):
+    new_img_data = []
+
+    for i, color in enumerate(img.convert("HSV").getdata()):
+        h, s, v = color
+        if (s <= 5 or s >= 204) and v > 127:
+            new_img_data.append((0, 0, 0))
+        elif s in range(56, 62) and v > 150:
+            new_img_data.append((0, 0, 0))
+        else:
+            new_img_data.append((255, 255, 255))
+
+    new_img = Image.new(img.mode, img.size)
+    new_img.putdata(new_img_data)
+    return new_img
+
+
+def make_screenshot(monitor_id):
+    with mss.mss() as sct:
+        monitor = sct.monitors[monitor_id]
+
+        sct_img = sct.grab(monitor)
+
+        img = Image.frombytes("RGB", (sct_img.size[0], round(sct_img.size[1] * 0.45)), sct_img.bgra, "raw", "BGRX")
+    return img
+
+
+def find_regex(expression, text):
+    regex = re.findall(expression, text)
+    if regex:
+        return regex[0]
+    return None
+
+
 def main():
     while True:
         if check_quit():
@@ -52,101 +86,33 @@ def main():
         legends = ["bloodhound", "gibraltar", "lifeline", "pathfinder", "octane",
                    "wraith", "bangalore", "caustic", "mirage"]
 
-        won_match = False
-        time_survived = ""
-        kills = 0
-        damage_done = 0
-        revives = 0
-        respawns = 0
-        solo = 0
-        legend = None
-        place = 0
-
-        if "won match" in text_ocr:
-            won_match = True
-        print("match won:", won_match)
-
-        regex = re.findall(r"[\n\r].*time survived [(\[]*([^\n\r)\]]*)", text_ocr)
-        if regex:
-            time_survived = regex[0]
-        else:
-            print("following not found:")
+        time_survived = find_regex(r"[\n\r].*time survived [(\[]*([^\n\r)\]]*)", text_ocr)
         print("time survived:", time_survived)
 
-        regex = re.findall(r"[\n\r].*kills [(\[]x*([^\n\r)\]]*)", text_ocr)
-        if regex:
-            kills = regex[0]
-        else:
-            print("following not found:")
+        kills = find_regex(r"[\n\r].*kills [(\[]x*([^\n\r)\]]*)", text_ocr)
         print("kills:", kills)
 
-        regex = re.findall(r"[\n\r].*damage done [(\[]*([^\n\r)\]]*)", text_ocr)
-        if regex:
-            damage_done = regex[0]
-        else:
-            print("following not found:")
+        damage_done = find_regex(r"[\n\r].*damage done [(\[]*([^\n\r)\]]*)", text_ocr)
         print("damage done:", damage_done)
 
-        regex = re.findall(r"[\n\r].*revive ally [(\[]x*([^\n\r)\]]*)", text_ocr)
-        if regex:
-            revives = regex[0]
-        else:
-            print("following not found:")
+        revives = find_regex(r"[\n\r].*revive ally [(\[]x*([^\n\r)\]]*)", text_ocr)
         print("revives:", revives)
 
-        regex = re.findall(r"[\n\r].*respawn ally [(\[]x*([^\n\r)\]]*)", text_ocr)
-        if regex:
-            respawns = regex[0]
-        else:
-            print("following not found:")
+        respawns = find_regex(r"[\n\r].*respawn ally [(\[]x*([^\n\r)\]]*)", text_ocr)
         print("respawns:", respawns)
 
-        regex = re.findall(r"[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
-        if regex:
-            solo = regex[0]
-        else:
-            print("following not found:")
+        solo = find_regex(r"[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
         print("solo:", solo)
 
-        regex = re.findall(r"[\n\r].*#*([^\n\r]*)", text_ocr)
-        if regex:
-            place = regex[0]
-        else:
-            print("following not found:")
-        print("place:", place)
+        placement = find_regex(r"[\n\r].*#*([^\n\r]*)", text_ocr)
+        print("place:", placement)
 
+        legend = None
         for legend_name in legends:
             if legend_name in text_ocr:
                 legend = legend_name
                 break
         print("legend:", legend)
-
-
-def clean_image(img):
-    new_img_data = []
-
-    for i, color in enumerate(img.convert("HSV").getdata()):
-        h, s, v = color
-        if (s <= 5 or s >= 204) and v > 127:
-            new_img_data.append((0, 0, 0))
-        elif s in range(56, 62) and v > 150:
-            new_img_data.append((0, 0, 0))
-        else:
-            new_img_data.append((255, 255, 255))
-
-    new_img = Image.new(img.mode, img.size)
-    new_img.putdata(new_img_data)
-    return new_img
-
-
-def make_screenshot(monitor_id):
-    with mss.mss() as sct:
-        monitor = sct.monitors[monitor_id]
-
-        sct_img = sct.grab(monitor)
-
-        img = Image.frombytes("RGB", (sct_img.size[0], round(sct_img.size[1] * 0.45)), sct_img.bgra, "raw", "BGRX")
-    return img
 
 
 if __name__ == "__main__":
