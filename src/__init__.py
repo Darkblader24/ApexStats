0b0ce84d585cbf59bfe0c7c1a928d1997e0dce5b
Lars Kue <lars@kuehmichel.de>
2019-03-20 14:22:07 +0100

    Changed Single Quotes to Double Quotes, modified gitignore

--- src/__init__.py
+++ src/__init__.py
@@ -7,93 +7,93 @@ from PIL import Image
 
 def main():
     # Make a screenshot of the selected monitor
-    img = make_screenshot(monitor_id=2)
+    # img = make_screenshot(monitor_id=2)
 
     # Remove noise from screenshot
-    cln_img = clean_image(img)
-    # cln_img = clean_image(Image.open('data/apex_screenshot_test_half.png'))
+    # cln_img = clean_image(img)
+    cln_img = clean_image(Image.open("data/apex_screenshot_test_half.png"))
 
     # Save screenshot and denoised image
-    # img.save('data/apex_screenshot.png')
-    cln_img.save('data/apex_stats_clean.png')
+    # img.save("data/apex_screenshot.png")
+    cln_img.save("data/apex_stats_clean.png")
 
     # Detect text from denoised screenshot
     text_ocr = pytesseract.image_to_string(cln_img).lower()
     print(text_ocr)
 
     # Handle text data
-    if 'xp breakdown' not in text_ocr:
-        print('No Apex Match Summary found!')
+    if "xp breakdown" not in text_ocr:
+        print("No Apex Match Summary found!")
         return
 
-    print('\n'
-          '\n'
-          '\n###########################'
-          '\n###### MATCH SUMMARY ######'
-          '\n###########################'
-          '\n')
+    print("\n"
+          "\n"
+          "\n###########################"
+          "\n###### MATCH SUMMARY ######"
+          "\n###########################"
+          "\n")
 
     won_match = False
-    time_survived = ''
+    time_survived = ""
     kills = 0
     damage_done = 0
     revives = 0
     respawns = 0
     solo = 0
-    legend = 'Bang Galore'
+    legend = "Bang Galore"
 
-    if 'won match' in text_ocr:
+    if "won match" in text_ocr:
         won_match = True
-    print('match won:', won_match)
+    print("match won:", won_match)
 
-    regex = re.findall('[\n\r].*time survived [(\[]*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*time survived [(\[]*([^\n\r)\]]*)", text_ocr)
     if regex:
         time_survived = regex[0]
     else:
-        print('following not found:')
-    print('time survived:', time_survived)
+        print("following not found:")
+    print("time survived:", time_survived)
 
-    regex = re.findall('[\n\r].*kills [(\[]x*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*kills [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         kills = regex[0]
     else:
-        print('following not found:')
-    print('kills:', kills)
+        print("following not found:")
+    print("kills:", kills)
 
-    regex = re.findall('[\n\r].*damage done [(\[]*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*damage done [(\[]*([^\n\r)\]]*)", text_ocr)
     if regex:
         damage_done = regex[0]
     else:
-        print('following not found:')
-    print('damage done:', damage_done)
+        print("following not found:")
+    print("damage done:", damage_done)
 
-    regex = re.findall('[\n\r].*revive ally [(\[]x*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*revive ally [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         revives = regex[0]
     else:
-        print('following not found:')
-    print('revives:', revives)
+        print("following not found:")
+    print("revives:", revives)
 
-    regex = re.findall('[\n\r].*respawn ally [(\[]x*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*respawn ally [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         respawns = regex[0]
     else:
-        print('following not found:')
-    print('respawns:', respawns)
+        print("following not found:")
+    print("respawns:", respawns)
 
-    regex = re.findall('[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         solo = regex[0]
     else:
-        print('following not found:')
-    print('solo:', solo)
+        print("following not found:")
+    print("solo:", solo)
 
-    regex = re.findall('[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)', text_ocr)
+    regex = re.findall("[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         legend = regex[0]
     else:
-        print('following not found:')
-    print('legend:', legend)
+        print("following not found:")
+    print("legend:", legend)
 
 
 
@@ -101,7 +101,7 @@ def main():
 def clean_image(img):
     new_img_data = []
 
-    for i, color in enumerate(img.convert('HSV').getdata()):
+    for i, color in enumerate(img.convert("HSV").getdata()):
         h, s, v = color
         if (s <= 5 or s >= 204) and v > 127:
             new_img_data.append((0, 0, 0))
@@ -125,5 +125,5 @@ def make_screenshot(monitor_id):
     return img
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
