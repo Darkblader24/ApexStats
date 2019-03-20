210832ff275db38e064757ce3b1f12fb4f5ee329
Lars Kue <lars@kuehmichel.de>
2019-03-20 14:25:38 +0100

    Fixed Reg expressions

--- src/__init__.py
+++ src/__init__.py
@@ -46,56 +46,54 @@ def main():
         won_match = True
     print("match won:", won_match)
 
-    regex = re.findall("[\n\r].*time survived [(\[]*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*time survived [(\[]*([^\n\r)\]]*)", text_ocr)
     if regex:
         time_survived = regex[0]
     else:
         print("following not found:")
     print("time survived:", time_survived)
 
-    regex = re.findall("[\n\r].*kills [(\[]x*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*kills [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         kills = regex[0]
     else:
         print("following not found:")
     print("kills:", kills)
 
-    regex = re.findall("[\n\r].*damage done [(\[]*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*damage done [(\[]*([^\n\r)\]]*)", text_ocr)
     if regex:
         damage_done = regex[0]
     else:
         print("following not found:")
     print("damage done:", damage_done)
 
-    regex = re.findall("[\n\r].*revive ally [(\[]x*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*revive ally [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         revives = regex[0]
     else:
         print("following not found:")
     print("revives:", revives)
 
-    regex = re.findall("[\n\r].*respawn ally [(\[]x*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*respawn ally [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         respawns = regex[0]
     else:
         print("following not found:")
     print("respawns:", respawns)
 
-    regex = re.findall("[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         solo = regex[0]
     else:
         print("following not found:")
     print("solo:", solo)
 
-    regex = re.findall("[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
+    regex = re.findall(r"[\n\r].*playing with friends [(\[]x*([^\n\r)\]]*)", text_ocr)
     if regex:
         legend = regex[0]
     else:
         print("following not found:")
     print("legend:", legend)
-
-
 
 
 def clean_image(img):
@@ -121,7 +119,7 @@ def make_screenshot(monitor_id):
 
         sct_img = sct.grab(monitor)
 
-        img = Image.frombytes("RGB", (sct_img.size[0], round(sct_img.size[1]*0.45)), sct_img.bgra, "raw", "BGRX")
+        img = Image.frombytes("RGB", (sct_img.size[0], round(sct_img.size[1] * 0.45)), sct_img.bgra, "raw", "BGRX")
     return img
 
 
